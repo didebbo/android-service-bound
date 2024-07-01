39e547769821c5f55eee9d6144a2b30e99095977
Gianluca Napoletano <gianluca@192.168.1.53>
2024-07-01 23:07:15 +0200

    refactoring

--- app/src/main/java/com/example/servicebound/AppService.kt
+++ app/src/main/java/com/example/servicebound/AppService.kt
@@ -1,56 +1,81 @@
 package com.example.servicebound
 
+
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlin.random.Random
 
 class AppService: Service() {
 
-    private var number: Int = 0
-    private var isBind: Boolean = false
+    private var threadID: Long = -1
+    private var _randomNumber: MutableLiveData<Int> = MutableLiveData<Int>(0)
+    val randomNumber: LiveData<Int> get() = _randomNumber
+
     private  var isRunning: Boolean = false
 
-    class MyServiceBinder: Binder() {
+    inner class AppServiceBinder: Binder() {
         fun getService(): AppService {
-            return AppService()
+            return this@AppService
         }
     }
 
-    private val myServiceBinder: MyServiceBinder = MyServiceBinder()
+    private val myServiceBinder: AppServiceBinder = AppServiceBinder()
 
     override fun onCreate() {
         super.onCreate()
-        println("[GN] onCreate MyService")
+        println("[GN] onCreate MyService on Thread $threadID")
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        println("[GN] onBind MyService")
-        isBind = true
+        println("[GN] onBind MyService on Thread $threadID")
         return  myServiceBinder
     }
 
     override fun onRebind(intent: Intent?) {
-        println("[GN] onRebind MyService")
-        isBind = true
+        println("[GN] onRebind MyService on Thread $threadID")
         super.onRebind(intent)
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
-        println("[GN] onUnbind MyService")
-        isBind = false
+        println("[GN] onUnbind MyService on Thread $threadID")
         return super.onUnbind(intent)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        println("[GN] onStartCommand MyService")
-        isRunning = true
-        return super.onStartCommand(intent, flags, startId)
+        if(!isRunning) {
+            println("[GN] onStartCommand MyService on Thread $threadID")
+            isRunning = true
+            Thread{
+                threadID = Thread.currentThread().id
+                println("[GN] Running Thread $threadID")
+                while (isRunning) {
+                    _randomNumber.apply {
+                        postValue(value?.plus(1))
+                    }
+                    println("[GN] Generated randomNumber: ${_randomNumber.value} on Thread $threadID")
+                    Thread.sleep(2000)
+                }
+                stopSelf()
+            }.start()
+        }
+        return START_STICKY
+    }
+
+    override fun stopService(name: Intent?): Boolean {
+        if(isRunning) {
+            println("[GN] stopService MyService on Thread $threadID")
+            isRunning = false
+        }
+        return super.stopService(name)
     }
 
     override fun onDestroy() {
-        println("[GN] onDestroy MyService")
-        isRunning = false
+        println("[GN] onDestroy MyService on Thread $threadID")
         super.onDestroy()
     }
 }
--- app/src/main/java/com/example/servicebound/MainActivity.kt
+++ app/src/main/java/com/example/servicebound/MainActivity.kt
@@ -1,16 +1,101 @@
 package com.example.servicebound
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.servicebound.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingView: ActivityMainBinding
+    private lateinit var appServiceIntent: Intent
+    private var appService: AppService? = null
+    private val _isAppServiceBound: MutableLiveData<Boolean> = MutableLiveData(false)
+    private  val isAppServiceBound: LiveData<Boolean> = _isAppServiceBound
+
+
+    private lateinit  var appServiceConnection:  ServiceConnection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        bindingView = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingView.root)
+
+        appServiceIntent = Intent(applicationContext,AppService::class.java)
+        appServiceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                val binder = service as AppService.AppServiceBinder
+                appService = binder.getService()
+                _isAppServiceBound.postValue(true)
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                appService = null
+                _isAppServiceBound.postValue(false)
+            }
+        }
+
+        updateTextPreview()
+
+        bindingView.startButton.setOnClickListener {
+            startAppService()
+        }
+        bindingView.stopButton.setOnClickListener {
+            stopAppService()
+        }
+        bindingView.bindButton.setOnClickListener {
+            bindAppService()
+        }
+        bindingView.unbindButton.setOnClickListener {
+            unbindAppService()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindAppService()
+    }
+
+    private fun updateTextPreview() {
+        isAppServiceBound.observe(this) { isAppServiceBound ->
+            println("[GN] updateTextPreview isAppServiceBound $isAppServiceBound")
+            when(isAppServiceBound) {
+                true -> appService?.randomNumber?.observe(this) {
+                    bindingView.previewTextView.text = it.toString()
+                }
+                false -> bindingView.previewTextView.text = "Service Not Found"
+            }
+        }
+    }
+    private fun startAppService() {
+        appService?.startService(appServiceIntent)
+    }
+
+    private fun stopAppService() {
+        appService?.stopService(appServiceIntent)
+    }
+    private fun bindAppService() {
+        if(isAppServiceBound.value == false) {
+            println("[GN] bindAppService")
+            bindService(appServiceIntent, appServiceConnection,Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    private fun unbindAppService() {
+        if(isAppServiceBound.value == true) {
+            println("[GN] unbindAppService")
+            appService?.randomNumber?.removeObservers(this)
+            unbindService(appServiceConnection)
+            _isAppServiceBound.postValue(false)
+            appService = null
+        }
     }
 }
